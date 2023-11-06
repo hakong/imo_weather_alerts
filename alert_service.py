778ed758e0933cfb6d083ca768fb6a7028f64613
Hákon Gíslason <hakon.gislason@gmail.com>
2023-11-06 16:09:25 +0000

    draft: stashing service work

--- alert_service.py
+++ alert_service.py
@@ -19,26 +19,27 @@ async def async_setup_alert_service(hass: HomeAssistant):
 
         if user_state is None:
             # Handle the error appropriately
-            _LOGGER.debug(f"Entity '{user_entity_id}' not found")
+            _LOGGER.error(f"Entity '{user_entity_id}' not found")
             return
         
         if alert_state is None:
             # Handle the error appropriately
-            _LOGGER.debug(f"Entity '{alert_entity_id}' not found")
+            _LOGGER.error(f"Entity '{alert_entity_id}' not found")
             return
         
-        # Assume that the user's state object has 'latitude' and 'longitude' attributes
         user_location = Point(float(user_state.attributes.get('latitude')), 
                               float(user_state.attributes.get('longitude')))
         _LOGGER.debug(f"User location: {user_location}")
 
-        # Get the alerts from the sensor's state object
         alerts = alert_state.attributes.get('alerts')
         if not alerts:
-            _LOGGER.debug(f"No alerts in alert_state.attributes '{alert_state}'")
+            _LOGGER.error(f"No alerts found in attribute 'alerts' of entity '{alert_entity_id}'")
             return
 
-        # Loop over each alert to check the polygons
+        # Initialize a list to store the identifiers of alerts where the user is in the area
+        user_in_alert_area_identifiers = []
+
+        # Loop over each alert to check the polygons and collect the identifiers
         for alert in alerts:
             _LOGGER.debug(f"Inside for alert in alerts")
             polygon_coords = alert.get('polygon')
@@ -50,14 +51,19 @@ async def async_setup_alert_service(hass: HomeAssistant):
                 _LOGGER.debug(f"alert_polygon: {alert_polygon}")
                 # Check if the user is in the alert area
                 if alert_polygon.contains(user_location):
-                    _LOGGER.debug(f"YES: alert_polygon contains user_location. User location: {user_location}")
+                    _LOGGER.debug(f"User is in alert area: {alert.get('identifier')} - {alert.get('areaDesc')}")
+                    user_in_alert_area_identifiers.append(alert.get('identifier'))
                 else:
-                    _LOGGER.debug(f"NO: alert_polygon does not container user_location. User location: {user_location}")
+                    _LOGGER.debug(f"NO: alert_polygon does not container user_location. User location: {user_location} - {alert.get('areaDesc')}")
+
+        # Do something with the list of identifiers, like logging, calling another service, etc.
+        _LOGGER.info(f"User '{user_entity_id}' is in the following alert areas: {user_in_alert_area_identifiers}")
+        # For example, you could return this list to the caller or store it somewhere.
+        # return user_in_alert_area_identifiers
 
     # Register our service with Home Assistant.
     hass.services.async_register(
-        'imo_weather_alerts',  # The domain where your service will live
-        'check_user_in_alert_area',  # The service name
-        handle_check_user_in_alert_area  # The callback function that is called when the service is executed
+        'imo_weather_alerts',
+        'check_user_in_alert_area',
+        handle_check_user_in_alert_area
     )
-
